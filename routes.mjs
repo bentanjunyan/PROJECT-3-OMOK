968f8501aa887007117e7974353e6a135482a8e8
bentanjunyan <bentanjunyan@gmail.com>
2022-04-05 15:15:48 +0800

    mkdir controllers

---
+++ routes.mjs
@@ -0,0 +1,9 @@
+// import db from './models/index.mjs';
+// import initUserController from './controllers/users.mjs';
+
+// export default function bindRoutes(app) {
+//   const UserController = initUserController(db);
+
+//   // all routes here
+//   app.get('/userInfo', UserController.userInfo);
+// }
