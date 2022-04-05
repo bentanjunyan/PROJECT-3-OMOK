00b4a2b95409b3dccec91e33b7da4fadec5251e0
bentanjunyan <bentanjunyan@gmail.com>
2022-04-05 15:59:30 +0800

    game logic: create win , draw conditions

---
+++ src/game.mjs
@@ -0,0 +1,220 @@
+// HELPER FUNCTION: GAME GRID SQUARE CLICK=======================================
+//=========================================================================
+const clickedGameSquare = (row, column) => {
+  const clickEffect = new Audio('./public/audio/click.mp3');
+  clickEffect.play();
+
+  if (omokGrid[row][column] === 0) {
+    omokGrid[row][column] = playerTurn;
+    if (playerTurn === 1) playerTurn = 2;
+    else playerTurn = 1;
+
+    renderGamePieces();
+    checkForWin(omokGrid, row, column);
+    win();
+  }
+};
+
+// HELPER FUNC: RENDER GAME BOARD ================================================
+//=========================================================================
+const renderGameBoard = () => {
+  const gameBackground = document.createElement('div');
+  gameBackground.classList = 'gameBackground';
+  gameBackground.style.width = `${(cellWidth * 19) + (gap * 19)}px`;
+  gameBackground.style.height = `${(cellWidth * 19) + (gap * 19)}px`;
+  gameBackground.style.backgroundColor = 'aliceblue';
+  gameBackground.style.position = 'absolute';
+  gameBackground.style.left = '0px';
+  gameBackground.style.right = '0px';
+  gameBackground.style.top = '0px';
+  gameBackground.style.bottom = '0px';
+
+  for (let row = 0; row < 19; row++) {
+    for (let column = 0; column < 19; column++) {
+      const gameSquare = document.createElement('div');
+      gameSquare.classList = 'gameSquare';
+      gameSquare.style.position = 'absolute';
+      gameSquare.style.width = `${cellWidth}px`;
+      gameSquare.style.height = `${cellWidth}px`;
+      gameSquare.style.backgroundColor = 'white';
+      gameSquare.addEventListener('click', () => { clickedGameSquare(`${row}`, `${column}`); });
+      gameSquare.addEventListener('click', () => { clickedGameSquare(`${row}`, `${column}`); });
+
+      gameSquare.addEventListener('mouseover', () => {
+        gameSquare.style.backgroundColor = 'rgb(240, 248, 255, 0.3)';
+      });
+
+      gameSquare.addEventListener('mouseout', () => {
+        gameSquare.style.backgroundColor = 'white';
+      });
+
+      gameSquare.style.left = `${(cellWidth + gap) * (column)}px`;
+      gameSquare.style.top = `${(cellWidth + gap) * (row)}px`;
+
+      gameBackground.appendChild(gameSquare);
+      gameUI.appendChild(gameBackground);
+    }
+  }
+};
+
+// HELPER FUNC: RENDER GAME PIECES ================================================
+//=========================================================================
+const renderGamePieces = () => {
+  for (let row = 0; row < 19; row++) {
+    for (let column = 0; column < 19; column++) {
+      const omokValue = omokGrid[row][column];
+      if (omokValue === 0) {
+        // empty
+      } else {
+        const omokPiece = document.createElement('div');
+        omokPiece.classList = 'omokPiece';
+        omokPiece.style.position = 'absolute';
+        omokPiece.style.width = `${cellWidth}px`;
+        omokPiece.style.height = `${cellWidth}px`;
+        omokPiece.style.left = `${(cellWidth + gap) * (column)}px`;
+        omokPiece.style.top = `${(cellWidth + gap) * (row)}px`;
+
+        if (omokValue === 1) {
+          // omokPiece.remove();
+          omokPiece.style.backgroundImage = "url('./public/images/slime.gif')";
+          omokPiece.style.backgroundRepeat = 'no-repeat';
+          omokPiece.style.backgroundSize = 'cover';
+        }
+        if (omokValue === 2) {
+          // omokPiece.remove();
+          omokPiece.style.backgroundImage = "url('./public/images/mushroom.gif')";
+          omokPiece.style.backgroundRepeat = 'no-repeat';
+          omokPiece.style.backgroundSize = 'cover';
+        }
+        gameUI.appendChild(omokPiece);
+      }
+    }
+  }
+};
+
+// HELPER FUNCTION: WIN + DRAW CONDITION CHECKERS===================================
+//=========================================================================
+const checkUp = (omokGrid, row, column) => {
+  if (Number(row) >= 4) {
+    if (omokGrid[row][column] !== 0 && omokGrid[row][column] === omokGrid[row - 1][column] && omokGrid[row][column] === omokGrid[row - 2][column] && omokGrid[row][column] === omokGrid[row - 3][column] && omokGrid[row][column] === omokGrid[row - 4][column]) {
+      winner = omokGrid[row][column];
+      console.log(`winner of round is ${winner}`);
+      //     return true;
+    }
+  }
+  // return false;
+};
+
+const checkDown = (omokGrid, row, column) => {
+  if (omokGridWidth - row >= 5) {
+    if (omokGrid[row][column] !== 0 && omokGrid[row][column] === omokGrid[row + 1][column] && omokGrid[row][column] === omokGrid[row + 2][column] && omokGrid[row][column] === omokGrid[row + 3][column] && omokGrid[row][column] === omokGrid[row + 4][column]) {
+      winner = omokGrid[row][column];
+      console.log(`winner of round is ${winner}`);
+      //     return true;
+    }
+  }
+  // return false;
+};
+
+const checkLeft = (omokGrid, row, column) => {
+  if (Number(column) >= 4) {
+    // console.log('omok piece', omokGrid[row][column]);
+    // console.log('location row', row);
+    // console.log('location col', column);
+    if (omokGrid[row][column] !== 0 && omokGrid[row][column] === omokGrid[row][column - 1] && omokGrid[row][column] === omokGrid[row][column - 2] && omokGrid[row][column] === omokGrid[row][column - 3] && omokGrid[row][column] === omokGrid[row][column - 4]) {
+      winner = omokGrid[row][column];
+      console.log(`winner of round is ${winner}`);
+      //     return true;
+    }
+  }
+  // return false;
+};
+
+const checkRight = (omokGrid, row, column) => {
+  if (omokGridWidth - column >= 5) {
+    // console.log('omok piece', omokGrid[row][column]);
+    // console.log('location row', row);
+    // console.log('location col', column);
+    if (omokGrid[row][column] !== 0 && omokGrid[row][column] === omokGrid[row][column + 1] && omokGrid[row][column] === omokGrid[row][column + 2] && omokGrid[row][column] === omokGrid[row][column + 3] && omokGrid[row][column] === omokGrid[row][column + 4]) {
+      winner = omokGrid[row][column];
+      console.log(`winner of round is ${winner}`);
+      //     return true;
+    }
+  }
+  // return false;
+};
+
+const checkDownRight = (omokGrid, row, column) => {
+  if ((omokGridWidth - row >= 5) && (omokGridWidth - column >= 5)) {
+    console.log('omok piece', omokGrid[row][column]);
+
+    if (omokGrid[row][column] !== 0 && omokGrid[row][column] === omokGrid[row + 1][column + 1] && omokGrid[row][column] === omokGrid[row + 2][column + 2] && omokGrid[row][column] === omokGrid[row + 3][column + 3] && omokGrid[row][column] === omokGrid[row + 4][column + 4]) {
+      winner = omokGrid[row][column];
+      console.log(`winner of round is ${winner}`);
+      //     return true;
+    }
+  }
+  // return false;
+};
+
+const checkDownLeft = (omokGrid, row, column) => {
+  if ((omokGridWidth - row >= 5) && (Number(column) >= 4)) {
+    console.log('omok piece', omokGrid[row][column]);
+
+    if (omokGrid[row][column] !== 0 && omokGrid[row][column] === omokGrid[row - 1][column + 1] && omokGrid[row][column] === omokGrid[row - 2][column + 2] && omokGrid[row][column] === omokGrid[row - 3][column + 3] && omokGrid[row][column] === omokGrid[row - 4][column + 4]) {
+      winner = omokGrid[row][column];
+      console.log(`winner of round is ${winner}`);
+      //     return true;
+    }
+  }
+  // return false;
+};
+
+const checkUpRight = (omokGrid, row, column) => {
+  if ((Number(row) >= 4) && (omokGridWidth - column >= 5)) {
+    console.log('omok piece', omokGrid[row][column]);
+
+    if (omokGrid[row][column] !== 0 && omokGrid[row][column] === omokGrid[row + 1][column - 1] && omokGrid[row][column] === omokGrid[row + 2][column - 2] && omokGrid[row][column] === omokGrid[row + 3][column - 3] && omokGrid[row][column] === omokGrid[row + 4][column - 4]) {
+      winner = omokGrid[row][column];
+      console.log(`winner of round is ${winner}`);
+      //     return true;
+    }
+  }
+  // return false;
+};
+
+const checkUpLeft = (omokGrid, row, column) => {
+  if ((Number(row) >= 4) && (Number(column) >= 4)) {
+    console.log('omok piece', omokGrid[row][column]);
+
+    if (omokGrid[row][column] !== 0 && omokGrid[row][column] === omokGrid[row - 1][column - 1] && omokGrid[row][column] === omokGrid[row - 2][column - 2] && omokGrid[row][column] === omokGrid[row - 3][column - 3] && omokGrid[row][column] === omokGrid[row - 4][column - 4]) {
+      winner = omokGrid[row][column];
+      console.log(`winner of round is ${winner}`);
+      //     return true;
+    }
+  }
+  // return false;
+};
+
+const checkDraw = (omokGrid) => {
+  for (let i = 0; i < 19; i++) {
+    if (omokGrid[0 + i].includes(0) === true) {
+      break;
+    } else {
+      winner = false;
+      console.log('draw');
+    }
+  }
+};
+
+const checkForWin = (omokGrid, row, column) => {
+  checkUp(omokGrid, Number(row), Number(column));
+  checkDown(omokGrid, Number(row), Number(column));
+  checkLeft(omokGrid, Number(row), Number(column));
+  checkRight(omokGrid, Number(row), Number(column));
+  checkDownLeft(omokGrid, Number(row), Number(column));
+  checkDownRight(omokGrid, Number(row), Number(column));
+  checkUpLeft(omokGrid, Number(row), Number(column));
+  checkUpRight(omokGrid, Number(row), Number(column));
+  checkDraw(omokGrid);
+};
